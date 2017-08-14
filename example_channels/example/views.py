615f036a84df9e89a038d5dd73097affdc3a9816
DesmondMM <mutsakaradesmond@gmail.com>
2017-08-14 23:30:14 +0100

    added signup html, view and url

--- example_channels/example/views.py
+++ example_channels/example/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import login, logout
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect
 
@@ -23,3 +23,15 @@ def log_in(request):
 def log_out(request):
     logout(request)
     return redirect(reverse('example:log_in'))
+
+
+def sign_up(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('example:log_in'))
+        else:
+            print(form.errors)
+    return render(request, 'example/sign_up.html', {'form': form})
