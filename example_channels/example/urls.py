2efb289ce00e7e439629f290a3c33e4b77af48ad
DesmondMM <mutsakaradesmond@gmail.com>
2017-08-14 02:40:43 +0100

    added a login html file, updated view and urls

--- example_channels/example/urls.py
+++ example_channels/example/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
-from example.views import user_list
+from example.views import log_in, log_out, user_list
 
 
 urlpatterns = [
-    url(r'^$', user_list, name='user_list'),
+    url(r'^log_in/$', log_in, name='log_in'),
+    url(r'^log_out/$', log_out, name='log_out'),
+    url(r'^$', user_list, name='user_list')
 ]
